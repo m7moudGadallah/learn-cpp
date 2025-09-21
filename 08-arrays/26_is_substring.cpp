35c3bcdfa7207606a119c6ff60213044c6a66cd2
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-09-21 19:04:09 +0300

    refactor: 26_is_substring.cpp

--- 08-arrays/26_is_substring.cpp
+++ 08-arrays/26_is_substring.cpp
@@ -14,31 +14,17 @@ int main() {
         return 0;
     }
 
-    if (word.size() == test_substr.size() and word != test_substr) {
-        cout << FAIL_MSG << '\n';
-        return 0;
-    }
+    bool is_valid_substr = false;
 
-    for (int i = 0; i <= word.size() - test_substr.size(); ++i) {
-        if (word[i] != test_substr.front())
-            continue;
+    for (int i = 0; i <= word.size() - test_substr.size() and !is_valid_substr; ++i) {
+        is_valid_substr = true; // assume it will be valid
 
-        bool is_valid_substr = true;
-
-        for (int j = 1; j < test_substr.size(); ++j) {
-            if (word[i + j] != test_substr[j]) {
-                is_valid_substr = false;
-                break;
-            }
-        }
-
-        if (is_valid_substr) {
-            cout << SUCCESS_MSG << '\n';
-            return 0;
+        for (int j = 1; is_valid_substr and j < test_substr.size(); ++j) {
+            is_valid_substr = (word[i + j] == test_substr[j]);
         }
     }
 
-    cout << FAIL_MSG << '\n';
+    cout << (is_valid_substr ? SUCCESS_MSG : FAIL_MSG) << '\n';
 
     return 0;
 }
