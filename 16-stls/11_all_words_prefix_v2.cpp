0a4031e6bb79265ec31eb500234117e0f8624be7
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-10-10 22:59:54 +0300

    feat: added 11_all_words_prefix_v2.cpp

---
+++ 16-stls/11_all_words_prefix_v2.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include<vector>
+#include<set>
+#include<unordered_map>
+
+using namespace std;
+
+struct TrieNode {
+public:
+    unordered_map<char, TrieNode*> children;
+    set<string> words;
+};
+
+struct Trie {
+private:
+    TrieNode* root;
+public:
+    Trie() {
+        root = new TrieNode();
+    }
+
+    void insert(const string& word) {
+        TrieNode* node = root;
+
+        for (const char& c : word) {
+            if (!node->children.count(c)) {
+                node->children[c] = new TrieNode();
+            }
+
+            node = node->children[c];
+            node->words.emplace(word);
+        }
+    }
+
+    vector<string> query_prefix(const string& prefix) {
+        TrieNode* node = root;
+
+        for (const auto& c : prefix) {
+            if (!node->children.count(c)) {
+                return {}; // prefix not found
+            }
+            node = node->children[c];
+        }
+
+        return vector<string>(node->words.begin(), node->words.end());
+    }
+};
+
+template<typename T>
+void print_vector(const vector<T>& vec) {
+    bool is_first = true;
+
+    cout << "[";
+
+    for (const auto& ele : vec) {
+        if (!is_first) {
+            cout << ", ";
+        }
+
+        is_first = false;
+        cout << ele;
+    }
+
+    cout << "]\n";
+}
+
+int main() {
+    int n; cin >> n;
+    string word;
+
+    Trie trie;
+
+    while (n--) {
+        cin >> word;
+        trie.insert(word);
+    }
+
+    int q; cin >> q;
+
+    string prefix;
+
+    while (q--) {
+        cin >> prefix;
+        print_vector(trie.query_prefix(prefix));
+    }
+    return 0;
+}
